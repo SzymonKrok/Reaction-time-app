d8a7efb7660fe77837512551db78e18b6744948b
SzymonKrok <szymonkrok49@gmail.com>
2022-12-11 18:45:32 +0100

    Added main functionality

--- main.js
+++ main.js
@@ -10,32 +10,43 @@ let randomNumber = 0,
 
 // Function start game - change button, rand number and set circle class
 const startGame = () => {
-    button.innerText = 'STOP!';
-    randomNumber = Math.floor(Math.random() * 5000) + 500;
+    button.innerText = 'PLAY AGAIN!';
+    result.innerText = '0';
+    circle.classList.remove('green');
     circle.classList.add('red');
+    randomNumber = Math.floor(Math.random() * 4000) + 500;
+
     setTimeout(() => {
         circle.classList.remove('red');
         circle.classList.add('green');
         startTime = new Date().getTime();
         console.log('Start time: ' + startTime)
-    }, randomNumber)
+    }, randomNumber);
+
 }
 
-//
+// Function which save time when you click circle
 
-const ReactionTime = () => {
+const reactionTime = () => {
     if(circle.classList.contains('green')){
         endTime = new Date().getTime();
         console.log('End time: ' + endTime);
-
     }
     resultFn()
 }
 
+// Function which compares both times
 const resultFn = () => {
     console.log(endTime - startTime);
     let timeLeft = endTime - startTime;
     result.innerText = timeLeft + 'ms';
+
+    //     Check that user doesn't click red circle
+
+    if (circle.classList.contains('red')){
+        alert("Too fast!");
+        result.innerText = '0ms'
+    }
 
 }
 
@@ -43,4 +54,4 @@ const resultFn = () => {
 
 
 button.addEventListener('click', startGame);
-circle.addEventListener('click', ReactionTime);
+circle.addEventListener('click', reactionTime);
